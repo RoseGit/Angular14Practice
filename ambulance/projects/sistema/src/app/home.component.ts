ce0a5d2c4c50b57540648d9e15bf5123b3572c8c
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-03-19 22:52:07 -0600

    Add variables to html

--- ambulance/projects/sistema/src/app/home.component.ts
+++ ambulance/projects/sistema/src/app/home.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
 /*Selector de etiqueta */
 @Component({
     selector: "amb-home",
-    template : "<h1>Home Page</h1>",
+    template : "<h1>Home Page</h1><span>{{title}}</span>",
     styles:['h1{ color:red; font-family:Verdana}', 'h1{font-size:150px}']
 })
 
@@ -23,4 +23,6 @@ import { Component } from "@angular/core";
 })*/
 
 //export es como decir que es publica
-export class HomeComponent{}
+export class HomeComponent{
+    title = 'Ambulance'
+}
