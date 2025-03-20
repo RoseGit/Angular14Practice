063679b58a25c843de5f4226bccb222694e21991
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-03-19 22:47:56 -0600

    Styles

--- ambulance/projects/sistema/src/app/home.component.ts
+++ ambulance/projects/sistema/src/app/home.component.ts
@@ -3,11 +3,12 @@ import { Component } from "@angular/core";
 /*Todas las vistas son componentes en Angualr */
 
 /*Selector de etiqueta */
-/*@Component({
+@Component({
     selector: "amb-home",
-    template : "<h1>Home Page</h1>"
+    template : "<h1>Home Page</h1>",
+    styles:['h1{ color:red; font-family:Verdana}', 'h1{font-size:150px}']
 })
-*/
+
 
 /*Selector de clase */
 /*@Component({
@@ -16,10 +17,10 @@ import { Component } from "@angular/core";
 })*/
 
 /*Selector de atributo */
-@Component({
-    selector: "[amb-home]",
+/*@Component({
+    selector: "amb-home",
     template : "<h1>Home Page</h1>"
-})
+})*/
 
 //export es como decir que es publica
 export class HomeComponent{}
