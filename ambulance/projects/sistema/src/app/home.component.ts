db848d5993fdd8135716dd21388d7f3f49744c0d
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-03-19 22:56:01 -0600

    Add table

--- ambulance/projects/sistema/src/app/home.component.ts
+++ ambulance/projects/sistema/src/app/home.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
 /*Selector de etiqueta */
 @Component({
     selector: "amb-home",
-    template : "<h1>Home Page</h1><span>{{title}}</span>",
+    template : "<h1>Home Page</h1><span>{{title}}</span><table><tr><th>Name</th></tr><tr><td>Rose</td></tr></table>",
     styles:['h1{ color:red; font-family:Verdana}', 'h1{font-size:150px}']
 })
 
