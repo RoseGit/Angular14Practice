b279aae5df15965c55df78e2c8af83c72bec4306
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-03-19 18:52:46 -0600

    replace home selector

---
+++ ambulance/projects/sistema/src/app/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+/*Todas las vistas son componentes en Angualr */
+@Component({
+    selector: "amb-home",
+    template : "<h1>Home</h1>"
+})
+
+//export es como decir que es publica
+export class HomeComponent{}
